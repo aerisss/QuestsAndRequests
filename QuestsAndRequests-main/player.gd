a1f9bc7b4c568e6deb19a40157707e0868856f4a
aerisss <70576770+aerisss@users.noreply.github.com>
2024-03-27 22:31:42 +0100

    Add files via upload
    dialog and character controller

---
+++ QuestsAndRequests-main/player.gd
@@ -0,0 +1,108 @@
+extends CharacterBody3D
+
+# Player nodes
+
+@onready var head = $head
+@onready var standing_collision_shape = $standing_collision_shape
+@onready var crouching_collision_shape = $crouching_collision_shape
+@onready var ray_cast_3d = $RayCast3D
+@onready var actionable_finder: Area3D = $Direction/ActionableFinder
+
+
+# Speed vars
+
+var current_speed = 5.0
+
+@export var walking_speed = 5.0
+@export var sprinting_speed = 8.0
+@export var crouching_speed = 3.0
+
+# Movement vars
+
+const JUMP_VELOCITY = 4.5
+
+var crouching_depth = -0.5
+
+var lerp_speed = 10.0
+
+# Input vars
+
+var direction = Vector3.ZERO
+const mouse_sens = 0.25
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# DIALOG BOX TEST 
+# !!! > Change Actionable to raycast  
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		var actionable =actionable_finder.get_overlapping_areas()
+		if actionable.size() > 0:
+			actionable[0].action()
+			return
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		
+		# Mouse looking logic
+		
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		
+		#head.rotate.x = clamp(head.rotation.x,deg_to_rad(-89),deg_to_rad(89))
+
+func _physics_process(delta):
+	
+	# Handle Movement state
+	
+	# Crouching
+	
+	if Input.is_action_pressed("crouch"):
+		
+		current_speed = crouching_speed
+		head.position.y = lerp(head.position.y,1.6 + crouching_depth,delta*lerp_speed)
+		
+		standing_collision_shape.disabled = true
+		crouching_collision_shape.disabled = false
+		
+	elif !ray_cast_3d.is_colliding():
+		
+	# Standing
+	
+		standing_collision_shape.disabled = false
+		crouching_collision_shape.disabled = true
+		
+		head.position.y = lerp(head.position.y,1.6,delta*lerp_speed)
+		
+		if Input.is_action_pressed("sprint"):
+			# Sprinting
+			current_speed = sprinting_speed
+		else: 
+			# Walking
+			current_speed = walking_speed
+			
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "Forward", "Backward")
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(),delta*lerp_speed)
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+
+	move_and_slide()
